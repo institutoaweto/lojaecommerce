156e200f163c04a7b95cbf2c6f0ac193b655e16c
institutoaweto <166181903+institutoaweto@users.noreply.github.com>
2024-04-20 10:55:38 -0300

    Add files via upload

--- js/unico.js
+++ js/unico.js
@@ -1,5 +1,5 @@
 function addproduto(nome, preco ,imagem, sinopse,pagina,imgum,imgdois,imgtres,imgquatro
-,valorantigo) {
+,valorantigo,pagamento) {
 
     sessionStorage.setItem('produto_nome', nome);
     sessionStorage.setItem('produto_preco', preco);
@@ -11,21 +11,27 @@ function addproduto(nome, preco ,imagem, sinopse,pagina,imgum,imgdois,imgtres,im
     sessionStorage.setItem('produto_imgtres', imgtres);
     sessionStorage.setItem('produto_imgquatro', imgquatro);
     sessionStorage.setItem('produto_valorantigo', valorantigo);
+    sessionStorage.setItem('produto_pagamento',pagamento);
+
+  
 
     window.location.href = 'unico.html';
 
 }
 
-const nome = sessionStorage.getItem('produto_nome');
-const preco = sessionStorage.getItem('produto_preco');
-const imagem = sessionStorage.getItem('produto_imagem');
-const sinopse = sessionStorage.getItem('produto_sinopse');
-const pagina = sessionStorage.getItem('produto_pagina');
-const imgum = sessionStorage.getItem('produto_imgum');
-const imgdois = sessionStorage.getItem('produto_imgdois');
-const imgtres = sessionStorage.getItem('produto_imgtres');
-const imgquatro = sessionStorage.getItem('produto_imgquatro');
-const valorantigo = sessionStorage.getItem('produto_valorantigo');
+let nome = sessionStorage.getItem('produto_nome');
+let preco = sessionStorage.getItem('produto_preco');
+let imagem = sessionStorage.getItem('produto_imagem');
+let sinopse = sessionStorage.getItem('produto_sinopse');
+let pagina = sessionStorage.getItem('produto_pagina');
+let imgum = sessionStorage.getItem('produto_imgum');
+let imgdois = sessionStorage.getItem('produto_imgdois');
+let imgtres = sessionStorage.getItem('produto_imgtres');
+let imgquatro = sessionStorage.getItem('produto_imgquatro');
+let valorantigo = sessionStorage.getItem('produto_valorantigo');
+let pagamento = sessionStorage.getItem('produto_pagamento');
+
+
 
 
 document.querySelector('.produto-detalhes .product-name').innerText = nome;
@@ -38,26 +44,13 @@ document.querySelector('.produto-detalhes .book-2').src = imgdois;
 document.querySelector('.produto-detalhes .book-3').src = imgtres;
 document.querySelector('.produto-detalhes .book-4').src = imgquatro;
 document.querySelector('.produto-detalhes .valor-antigo').innerHTML = valorantigo;
+document.querySelector('.produto-detalhes .pagamento-final').href = pagamento;
 
 
 
 
-/*
-function adicionarProduto(nome, preco ,imagem) {
-
-    sessionStorage.setItem('produto_nome', nome);
-    sessionStorage.setItem('produto_preco', preco);
-    sessionStorage.setItem('produto_imagem', imagem);
-
-    window.location.href = 'unico.html';
-}
 
 
-const nome = sessionStorage.getItem('produto_nome');
-const preco = sessionStorage.getItem('produto_preco');
-const imagem = sessionStorage.getItem('produto_imagem');
 
-document.querySelector('.produto-detalhes .product-name').innerText = nome;
-document.querySelector('.produto-detalhes .product-price').innerText = `R$${preco}`;
-document.querySelector('.produto-detalhes .imagem-produto').src = imagem;
-*/
+
+
